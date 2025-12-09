5029f901f2afc75577a4e88bd04f02979728a823
Vinay <your_email@example.com>
2025-12-09 13:42:16 +0530

    done

--- app/lib/constant.js
+++
@@ -1,6 +0,0 @@
-export const API_BASE =
-  typeof window !== "undefined" && window.Capacitor
-    ? "https://clg-bus-management-gc6w-ih9w6pqgv-sssss1122-cells-projects.vercel.app"
-    : process.env.NODE_ENV === "development"
-    ? ""
-    : "https://clg-bus-management-gc6w-ih9w6pqgv-sssss1122-cells-projects.vercel.app";
